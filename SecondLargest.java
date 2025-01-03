c49eb1b53742e83d20d203155e40edc78641c06f
Devavarshita <devavarshita2311@gmail.com>
2025-01-03 22:27:42 +0530

    Update and rename Solution.java to SecondLargest.java
    Added comments wherever necessary to give detail explanations.

--- SecondLargest.java
+++ SecondLargest.java
@@ -1,7 +1,11 @@
-class Solution {
+//Arrays - Day_1
+//This function will return the second largest element in an array.
+//If no such element exists then it will return -1, this case will occur when the all the elements in the array are same.
+class SecondLargest {
     public int getSecondLargest(int[] arr) {
-        // Code Here
+        //Consider the first element be the largest.
         int max = arr[0];
+        //This loop will find the largest element in the array.
         for(int i=0;i<arr.length;i++)
         {
             if(arr[i]>max)
@@ -9,6 +13,7 @@ class Solution {
                 max = arr[i];
             }
         }
+        // The largest element wherever occurs will be replaced by -1.
         for(int i=0;i<arr.length;i++)
         {
             if(arr[i]==max)
@@ -16,6 +21,8 @@ class Solution {
                 arr[i]=-1;
             }
         }
+        // Now as the largest element is as replaced by -1.
+        //Finding the largest will give the second largest.
         max = arr[0];
         for(int i=0;i<arr.length;i++)
         {
